7f43f14ac0bcbe5d23403e06a5791c20a0028c89
liuyue-feilengxue <62792199+liuyue-feilengxue@users.noreply.github.com>
2020-06-15 19:27:37 +0800

    0.0.1.2
    login

--- miniprogram/pages/my/my.js
+++ miniprogram/pages/my/my.js
@@ -5,14 +5,38 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    userinfo:{},
+    openid:""
+  },
+  onGotUserInfo(e){
+    const that = this
+    wx.cloud.callFunction({
+      name:"login",
+      success:res=>{
+        that.setData({
+          openid: res.result.openid,
+          userinfo : e.detail.userInfo
+        })
+        //保存在缓存中
+        that.data.userinfo.openid = that.data.openid
+        // console.log(userInfo)
+        wx.setStorageSync('userinfo', that.data.userinfo)
+      },
+      fail:res=>{
+        console.log("云函数调用失败")
+      }
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    const ui=wx.getStorageSync('userinfo')
+    this.setData({
+      userinfo: ui,
+      openid: ui.openid
+    })
   },
 
   /**

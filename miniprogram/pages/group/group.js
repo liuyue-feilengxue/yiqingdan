18360839694c3e2ccf168601a95a052ee10a2ea7
liuyue-feilengxue <62792199+liuyue-feilengxue@users.noreply.github.com>
2020-06-24 14:45:51 +0800

    0.0.2.4

--- miniprogram/pages/group/group.js
+++ miniprogram/pages/group/group.js
@@ -5,7 +5,29 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    inputShowed: false,
+    inputVal: ""
+  },
+  showInput: function () {
+    this.setData({
+        inputShowed: true
+    });
+  },
+  hideInput: function () {
+    this.setData({
+        inputVal: "",
+        inputShowed: false
+    });
+  },
+  clearInput: function () {
+    this.setData({
+        inputVal: ""
+    });
+  },
+  inputTyping: function (e) {
+    this.setData({
+        inputVal: e.detail.value
+    });
   },
   
   /**

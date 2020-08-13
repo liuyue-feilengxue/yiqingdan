c8c557077cfab729a08b6c43046d8eee77bb7e55
liuyue-feilengxue <62792199+liuyue-feilengxue@users.noreply.github.com>
2020-08-13 21:17:16 +0800

    update

--- miniprogram/pages/groupGroupTaskDetail/groupGroupTaskDetail.js
+++ miniprogram/pages/groupGroupTaskDetail/groupGroupTaskDetail.js
@@ -15,7 +15,17 @@ Page({
     // 未完成任务的人
     unfinish:[],
     // 本群
-    fGroup:{}
+    fGroup:{},
+    // 是否为管理
+    isAdmin:false
+  },
+
+  // 把已完成改为未完成
+  changeToUnfinish(e){
+    var index = e.currentTarget.dataset.index
+    var finish = this.data.finish
+    finish.splice(index,1)
+    console.log(finish)
   },
 
   /**
@@ -24,42 +34,10 @@ Page({
   onLoad: function (options) {
     var fGroupNum = Number(options.fGroupNum)
     var fTask = JSON.parse(options.taskjson)
-    const that = this
-    // 已经完成任务的人
-    var finish = []
-    for (let i=0;i<fTask.fFinish.length;i++){
-      finish.push(fTask.fFinish[i])
-    }
     this.setData({
       fGroupNum:fGroupNum,
       fTask:fTask,
-      finish:finish
-    })
-    
-    wx.cloud.callFunction({
-      name:"getTGroup",
-      data:{
-        fGroupNum:fGroupNum
-      }
-    }).then(res=>{
-      var fGroup = res.result.data[0]
-      var allMember = fGroup.fAdministrator.concat(fGroup.fMember)
-      // 此处可能有bug
-      for (let i=0;i<allMember.length;i++){
-        for (let j=0;j<finish.length;j++){
-          if (allMember[i].openid == finish[j].openid){
-            allMember.splice(i,1)
-            i--
-          }
-        }
-      }
-      // 设置unfinish
-      that.setData({
-        unfinish:allMember,
-        fGroup:fGroup
-      })
-    })
-
+    }) 
   }, 
 
   /**
@@ -73,7 +51,55 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    wx.showLoading({
+      title: '加载中',
+    })
+    const that = this
+    const ui = wx.getStorageSync('userinfo')
+    // 已经完成任务的人
+    var finish = []
+    for (let i=0;i<that.data.fTask.fFinish.length;i++){
+      finish.push(that.data.fTask.fFinish[i])
+    }
+    this.setData({
+      finish:finish
+    })
+    // getTGroup
+    wx.cloud.callFunction({
+      name:"getTGroup",
+      data:{
+        fGroupNum:that.data.fGroupNum
+      }
+    }).then(res=>{
+      var fGroup = res.result.data[0]
+      var allMember = fGroup.fAdministrator.concat(fGroup.fMember)
+      // 此处可能有bug
+      for (let i=0;i<allMember.length;i++){
+        for (let j=0;j<finish.length;j++){
+          if (allMember[i].openid == finish[j].openid){
+            allMember.splice(i,1)
+            i--
+            break
+          }
+        }
+      }
+      // 是否为管理员（删除该同学完成任务）
+      var isAdmin = false
+      for (let i=0;i<fGroup.fAdministrator.length;i++){
+        // 是管理
+        if (fGroup.fAdministrator[i].openid == ui.openid){
+          isAdmin = true
+          break
+        }
+      }
+      // 设置unfinish
+      that.setData({
+        unfinish:allMember,
+        fGroup:fGroup,
+        isAdmin:isAdmin
+      })
+      wx.hideLoading()
+    })
   },
 
   /**
